eb1916524d433c2616eaef644223c866d5ad9692
Abdullah A Saleem <abdullah3333003@gmail.com>
2018-05-18 02:30:17 +0300

    Update GreenAdapter.java

--- app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -33,19 +33,32 @@ import android.widget.TextView;
  * contents are green.
  */
 // TODO (4) From GreenAdapter, extend RecyclerView.Adapter<NumberViewHolder>
-public class GreenAdapter {
+public class GreenAdapter extends RecyclerView.Adapter <GreenAdapter.NumberView Holder> {
+    
 
     // TODO (1) Add a private int variable called mNumberItems
 
     // TODO (2) Create a constructor for GreenAdapter that accepts an int as a parameter for numberOfItems
     // TODO (3) Store the numberOfItems parameter in mNumberItems
-
+public class GreenAdapter(int numbrOfItems) { mNumber Items=numberOfItems;
+                                            }
     // TODO (5) Override the onCreateViewHolder method
     // TODO (6) Create and return a new NumberViewHolder within this method
-
+@Override
+    Public NumberViewHolder onCraeteViewHolder(viewGroup viewGroup,int view type) {
+        Context context=viewGroup.getcontext();
+        int layuotinflater inflater=layout Inflater.from(context);
+        boolean shouldAttachToParentImmediately=false;
+        view view=infater,inflate(layoutIdForlistItem,
+                                  view Group.ShoulaAttochToParentImmediately);
+        NumberViewHolder=new NumberViewHolder(view);
+        return viewHolder; }
     // TODO (7) Override onBindViewHolder
     // TODO (8) Within onBindViewHolder, call holder.bind and pass in the position
-
+@Override
+    Public void onBinViewHolder(NumberViewHolder holder, int position){
+        holder.bind (position);
+    }
     // TODO (9) Override getItemCount and return the number of items to display
 
     /**
